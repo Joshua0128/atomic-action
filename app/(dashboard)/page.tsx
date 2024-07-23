143b3139a7f97b5cca2009632b69ef59c01cb7bf
Wei Ting Wu <weiting@projectmars.info>
2024-07-23 09:06:11 +0800

    init: init the next project and setup clerk auth

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>UnProtected Landing Page</h1>
+			<p>Unprotecting...</p>
+		</div>
+	)
+}
---
+++ app/plan/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>Protected Landing Page</h1>
+			<p>protecting...</p>
+		</div>
+	)
+}
